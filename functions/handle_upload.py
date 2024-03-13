507cce61c0514aa3b739d88da58391f0804f7a33
Trevor Grant <trevor.d.grant@gmail.com>
2024-03-12 20:15:35 -0500

    bug tracking hf feed issues

--- functions/handle_upload.py
+++ functions/handle_upload.py
@@ -16,7 +16,7 @@ def handle_upload(bucket_name, file_name, db, verbose=True):
     # Get the bucket and file objects
     bucket = storage_client.get_bucket(bucket_name)
     file = bucket.blob(file_name)
-
+    if verbose: logger.log(f"File: {file_name}")
     user_uid = file_name.split('/')[0]
     file_name_wo_path = ''.join(file_name.split('/')[-1])
 
@@ -26,10 +26,10 @@ def handle_upload(bucket_name, file_name, db, verbose=True):
     #  2. update created field of user to include ref to id of empty object created
     user_ref = db.collection('users').document(user_uid)
     user_name = user_ref.get().get('displayName')
-    if verbose: print(f"User Name: {user_name}")
+    if verbose: logger.log(f"User Name: {user_name}")
     uuid_value = file_name.split('/')[1].split('.')[0]
     if verbose:
-        print(f"uid: {user_uid} creating {uuid_value} for {user_name}")
+        logger.log(f"uid: {user_uid} creating {uuid_value} for {user_name}")
         logger.log(f"uid: {user_uid} creating {uuid_value} for {user_name}")
     output = {'id': uuid_value, 's3': {'bucket': bucket_name, 'key': file_name}, 'ratings': [], 'saves': [], 'tags': [],
               'short_description': '', 'avg_rating': 0.00, 'n_saves': 0, 'created': gc_firestore.SERVER_TIMESTAMP,
@@ -39,7 +39,7 @@ def handle_upload(bucket_name, file_name, db, verbose=True):
     doc_ref.set(output)
     user_ref = db.collection('users').document(user_uid)
     user_data = user_ref.get().to_dict()
-    # if verbose: print(f"user_data: ", user_data)
+    # if verbose: logger.log(f"user_data: ", user_data)
     created_by_data = user_data.get('created_by', {})
     created_by_data[output['id']] = {'title': "Processing...", 'status': output['status']}
     user_ref.update({'created_by': created_by_data})
@@ -53,7 +53,7 @@ def handle_upload(bucket_name, file_name, db, verbose=True):
         if verbose: logger.log('Ends with ".pdf" processing as journal article.')
         output2 = process_article(temp_file, doc_ref, verbose)
     elif file_name.endswith(".epub"):
-        if verbose: print("epub")
+        if verbose: logger.log("epub")
         book_dict = process_epub_to_book_dict(temp_file)
         logger.log("Book Dict Created: " + book_dict['title'])
         output2 = book_dict_to_markdown(book_dict)
